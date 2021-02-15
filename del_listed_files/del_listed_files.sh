0f607ea4e83f4d8d1d4b2a44b4dbf34f64300d17
Alpha <lukasnmd@gmail.com>
2021-02-14 21:39:53 -0300

    Update del_listed_files.sh

--- del_listed_files/del_listed_files.sh
+++ del_listed_files/del_listed_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #xxxxxxxxxxxxxxx del_listed_files_script xxxxxxxxxxxxxxxx
 
+cd /home/container
+
 # Please create a file named rmlist.txt and feed it with
 # the full path to each file you wish to delete.
 
@@ -9,7 +11,7 @@ if [[ $RCSVAR == 'true' ]]; then
 		rm -f $i
 	done
 else
-	echo Majestic Host custom script disabled.
+	echo rmlist.txt file is missing, please check on file manager root directory.
 fi
 
 # Wrote_by_Veckia_(Alpha#9751_at_discord)
